5dc4b55f6a3141d7065e61825b096c35ec34ae45
wild7 <wild7_lief25@yahoo.co.jp>
2017-11-05 18:42:05 +0900

    Add: #2 現在登録されているブックマークのログを表示するよう実装

--- background.js
+++ background.js
@@ -1,4 +1,41 @@
-var DELAY = 1;
+var DELAY = 0.1;
+
+// ブックマーク走査
+class BookmarkTracer {
+  constructor() {
+    this.name = 'ExtendBookmarks';
+  }
+
+  // ブックマークツリーを走査
+  traceTree(bookmarkItems) {
+    this.traceItems(bookmarkItems[0], 0);
+  };
+
+  // ブックマークアイテムを走査
+  traceItems(bookmarkItem, indent)
+  {
+    if (bookmarkItem.children) {
+      this.displayLog(bookmarkItem, indent);
+      indent++;
+      for (let childlen of bookmarkItem.children) {
+        this.traceItems(childlen, indent);
+      }
+    } else {
+      this.displayLog(bookmarkItem, indent);
+    }
+    indent--;
+  }
+
+  // ログを表示
+  displayLog(bookmarkItem, indent) {
+    console.log(this.appendIndent(indent) + bookmarkItem.title);
+  }
+
+  // インデントを追加
+  appendIndent(indentLength) {
+    return " ".repeat(indentLength) + ">";
+  }
+}
 
 createAlarm();
 
@@ -13,4 +50,7 @@ function createAlarm()
 // アラーム動作
 browser.alarms.onAlarm.addListener((alarm) => {
   console.log('onAlarms');
+
+  let bookmarkTracer = new BookmarkTracer();
+  browser.bookmarks.getTree(bookmarkTracer.traceTree.bind(bookmarkTracer));
 });
